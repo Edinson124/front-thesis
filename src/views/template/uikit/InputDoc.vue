d6b492296e720cd95fb9659be2fa4f65eeaa40d1
Edinson Cotrina <developerwin-04@win.pe>
2025-05-14 17:14:29 -0500

    Save services in service

--- src/views/template/uikit/InputDoc.vue
+++ src/views/template/uikit/InputDoc.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { CountryService } from '@/service/CountryService';
-import { NodeService } from '@/service/NodeService';
+import { CountryService } from '@/services/service/CountryService';
+import { NodeService } from '@/services/service/NodeService';
 import { onMounted, ref } from 'vue';
 
 const floatValue = ref(null);
--- src/views/template/uikit/TableDoc.vue
+++ src/views/template/uikit/TableDoc.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { CustomerService } from '@/service/CustomerService';
-import { ProductService } from '@/service/ProductService';
+import { CustomerService } from '@/services/service/CustomerService';
+import { ProductService } from '@/services/service/ProductService';
 import { FilterMatchMode, FilterOperator } from '@primevue/core/api';
 import { onBeforeMount, reactive, ref } from 'vue';
 
